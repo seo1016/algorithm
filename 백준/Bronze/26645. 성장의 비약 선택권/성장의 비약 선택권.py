0b470e98482ed121a2bb90f173f199cdfed0c407
seo1016 <121356780+seo1016@users.noreply.github.com>
2023-04-03 12:45:32 +0900

    [Bronze III] Title: 성장의 비약 선택권, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26645. 성장의 비약 선택권/성장의 비약 선택권.py
@@ -0,0 +1,15 @@
+N = int(input())
+if 200<=N+8<=213:
+    print(1)
+elif N+8 == 214:
+    print(2)
+elif 210<=N+4<=221:
+    print(2)
+elif N+4 == 222:
+    print(3)
+elif 220<=N+2<=230:
+    print(3)
+elif N+2 == 231:
+    print(4)
+else:
+    print(4)
