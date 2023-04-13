9be9e2c0414150da6de33d4a3723d837bf07fc23
ssh <121356780+seo1016@users.noreply.github.com>
2023-04-13 10:23:02 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+c, d, e, f, g = map(int, input().split())
+if a*b-c != 0:
+    print(-(a*b-c), end = ' ')
+else:
+    print('0', end = ' ')
+if a*b-d != 0:
+    print(-(a*b-d), end = ' ')
+else:
+    print('0', end = ' ')
+if a*b-e != 0:
+    print(-(a*b-e), end = ' ')
+else:
+    print('0', end = ' ')
+if a*b-f != 0:
+    print(-(a*b-f), end = ' ')
+else:
+    print('0', end = ' ')
+if a*b-g != 0:
+    print(-(a*b-g), end = ' ')
+else:
+    print('0', end = ' ')
