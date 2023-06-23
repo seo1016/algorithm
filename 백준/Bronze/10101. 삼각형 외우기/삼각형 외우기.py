a2c21d6d700821499aa695f03a393029fb4aa483
ssh <121356780+seo1016@users.noreply.github.com>
2023-06-23 22:34:49 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a == 60 and b == 60 and c == 60:
+    print("Equilateral")
+elif a+b+c == 180 and a == b:
+    print("Isosceles")
+elif a+b+c == 180 and a == c:
+    print("Isosceles")
+elif a+b+c == 180 and b == c:
+    print("Isosceles")
+elif a+b+c == 180 and a != b != c:
+    print("Scalene")
+elif a+b+c != 180:
+    print("Error")
