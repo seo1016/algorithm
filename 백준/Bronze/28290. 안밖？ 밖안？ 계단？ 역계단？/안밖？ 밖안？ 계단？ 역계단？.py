cab87de2aefe28ca4ff23a630c8e9b4dc7765d14
ssh <121356780+seo1016@users.noreply.github.com>
2023-07-11 19:14:10 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.py
@@ -0,0 +1,11 @@
+tabub = input()
+if tabub == 'fdsajkl;' or tabub == 'jkl;fdsa':
+    print("in-out")
+elif tabub == 'asdf;lkj' or tabub == ';lkjasdf':
+    print("out-in")
+elif tabub == 'asdfjkl;':
+    print("stairs")
+elif tabub == ';lkjfdsa':
+    print("reverse")
+else:
+    print("molu")
