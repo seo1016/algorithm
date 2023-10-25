8e96427c8ebf9a942fc509863713fa42b8161b55
서승훈 <seoseunghun3@gmail.com>
2023-10-25 17:57:01 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    long long int A = 0;
+    long long int B = 0;
+    long long int C = 0;
+    scanf("%lld %lld %lld", &A, &B, &C);
+    printf("%lld", A+B+C);
+}
