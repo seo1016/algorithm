b8fd70fd111c90cf9ae537d51c392f019b90c38f
서승훈 <seoseunghun3@gmail.com>
2023-10-25 17:12:43 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.c
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-    long double a = 0;
-    long double b = 0;
-    scanf("%LG %LG", &a, &b);
-    printf("%.100LG", a/b);
+    double a = 0;
+    double b = 0;
+    scanf("%lf %lf", &a, &b);
+    printf("%.100lf", a/b);
     return 0;
 }
