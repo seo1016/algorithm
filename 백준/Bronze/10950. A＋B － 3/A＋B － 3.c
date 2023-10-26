b617a79820f39b174abf01396598554d3ceb2196
서승훈 <seoseunghun3@gmail.com>
2023-10-26 23:41:12 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int t = 0;
+    int a = 0;
+    int b = 0;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
