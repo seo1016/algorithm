0e70673b901eca38aa307335eee7e9593f446a77
서승훈 <seoseunghun3@gmail.com>
2024-05-21 20:58:55 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,14 +1,18 @@
-t = int(input())
-for i in range(t):
-    a = input()
-    arr = []
-    for j in range(len(a)):
-        arr.append(a[j])
-        if len(arr) >= 2:
-            if arr[-2:] == ["(",")"]:
-                arr.pop()
-                arr.pop()
-    if arr == []:
-        print("YES")
-    else:
-        print("NO")
+n = int(input())
+cnt = 0
+cntt = 0
+for i in range(n):
+	a = input()
+	for j in a:
+		if j=="(":
+			cnt+=1
+		else:
+			cnt-=1
+		if cnt<0:
+			cntt+=1
+	if cnt==0 and cntt==0:
+		print("YES")
+	else:
+		print("NO")
+	cnt = 0
+	cntt=0
