f4e33e0c6474a50d1a2fc6e225b2b2d6587feab7
서승훈 <seoseunghun3@gmail.com>
2025-03-24 17:24:07 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,18 +1,22 @@
 n = int(input())
-cnt = 0
-cntt = 0
 for i in range(n):
-	a = input()
-	for j in a:
-		if j=="(":
-			cnt+=1
-		else:
-			cnt-=1
-		if cnt<0:
-			cntt+=1
-	if cnt==0 and cntt==0:
-		print("YES")
-	else:
-		print("NO")
-	cnt = 0
-	cntt=0
+    left = 0
+    right = 0
+    isTrue = True
+    
+    string = input()
+    string = list(string)
+    
+    for j in string:
+        if j == '(':
+            left += 1
+        else:
+            if left != 0:
+                left -= 1
+            else:
+                isTrue = False
+
+    if isTrue and left == 0 and right == 0:
+        print("YES")
+    else:
+        print("NO")
