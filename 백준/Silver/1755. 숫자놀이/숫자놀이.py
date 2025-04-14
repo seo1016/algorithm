ebdd87e23a11a27533764b13ec7bb210d2455240
서승훈 <seoseunghun3@gmail.com>
2025-04-14 14:57:10 +0900

    [Silver IV] Title: 숫자놀이, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+dic = {
+    '0': 'zero',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine'
+}
+
+arr = []
+for i in range(M, N+1):
+    word = ' '.join(dic[d] for d in str(i))
+    arr.append((word, i))
+
+arr.sort()
+
+for i in range(len(arr)):
+    print(arr[i][1], end=' ')
+    if (i + 1) % 10 == 0:
+        print()
