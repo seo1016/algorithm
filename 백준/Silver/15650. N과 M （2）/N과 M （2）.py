0df71965088f3c7d5162193bdc8ffa2a0b7e7f7c
서승훈 <seoseunghun3@gmail.com>
2025-04-19 15:01:57 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,14 @@
+def backtrack(start, depth):
+    if depth == M:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(start, N + 1):
+        result.append(i)
+        backtrack(i + 1, depth + 1)
+        result.pop()
+
+N, M = map(int, input().split())
+result = []
+
+backtrack(1, 0)
