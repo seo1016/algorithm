df8bd41978790474e1b65dcff7e171d5658beacf
서승훈 <seoseunghun3@gmail.com>
2025-06-30 09:12:07 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,35 +1,7 @@
-a = input()
-b = 0
-n = 0
-arr = []
-for i in a:
-    arr.append(i)
-while b < len(arr):
-    if b+2 <= len(arr) and arr[b] == 'c' and arr[b+1] == '=':
-        n += 1
-        b += 2
-    elif b+2 <= len(arr) and arr[b] == 'c' and arr[b+1] == '-':
-        n += 1
-        b += 2
-    elif b+2 <= len(arr) and arr[b] == 'd' and arr[b+1] == '-':
-        n += 1
-        b += 2
-    elif b+3 <= len(arr) and arr[b] == 'd' and arr[b+1] == 'z' and arr[b+2] == '=':
-        n += 1
-        b += 3
-    elif b+2 <= len(arr) and arr[b] == 'l' and arr[b+1] == 'j':
-        n += 1
-        b += 2
-    elif b+2 <= len(arr) and arr[b] == 'n' and arr[b+1] == 'j':
-        n += 1
-        b += 2
-    elif b+2 <= len(arr) and arr[b] == 's' and arr[b+1] == '=':
-        n += 1
-        b += 2
-    elif b+2 <= len(arr) and arr[b] == 'z' and arr[b+1] == '=':
-        n += 1
-        b += 2
-    else:
-        n += 1
-        b += 1
-print(n)
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+alphabet = input()
+
+for i in croatia:
+    alphabet = alphabet.replace(i, '.')
+
+print(len(alphabet))
