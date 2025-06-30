454d350330ad8d1a64776502e7206f3ee7a6183f
서승훈 <seoseunghun3@gmail.com>
2025-06-30 09:58:26 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,51 +1,11 @@
-a = int(input())
-b = int(input())
-c = int(input())
+A = int(input())
+B = int(input())
+C = int(input())
+arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-arr = [0 for i in range(10)]
+num = str(A*B*C)
+for i in num:
+    arr[int(i)] += 1
 
-d = 0
-e = 0
-f = 0
-g = 0
-h = 0
-k = 0
-l = 0
-m = 0
-n = 0
-o = 0
-z = a*b*c
-z = str(z)
-
-for j in z:
-    if j == '0':
-        d += 1
-    elif j == '1':
-        e += 1
-    elif j == '2':
-        f += 1
-    elif j == '3':
-        g += 1
-    elif j == '4':
-        h += 1
-    elif j == '5':
-        k += 1
-    elif j == '6':
-        l += 1
-    elif j == '7':
-        m += 1
-    elif j == '8':
-        n += 1
-    elif j == '9':
-        o += 1
-
-print(d)
-print(e)
-print(f)
-print(g)
-print(h)
-print(k)
-print(l)
-print(m)
-print(n)
-print(o)
+for i in arr:
+    print(i)
