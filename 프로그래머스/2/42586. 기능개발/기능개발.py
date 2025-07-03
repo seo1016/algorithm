354c75d00fa7ed2fcd71a9a2587abf52abe31811
서승훈 <seoseunghun3@gmail.com>
2025-07-03 21:39:49 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,40 +1,16 @@
+import math
+from collections import Counter
+
 def solution(progresses, speeds):
-    answer = []
     arr = []
     for i in range(len(progresses)):
-        end = progresses[i]
-        time = 0
-        while True:
-            if end >= 100:
-                maxx = 0
-                if len(arr) == 0:
-                    arr.append(time)
-                else:
-                    for i in arr:
-                        if i > time:
-                            maxx = i
-                        else:
-                            continue
-                    if maxx == 0:
-                        arr.append(time)
-                    else:
-                        arr.append(maxx)
-                break
-            else:
-                end += speeds[i]
-                time += 1
-                
-    arr.sort() # 7, 7, 9
-    print(arr)
-    
-    num = 1
+        arr.append(math.ceil((100-progresses[i])/speeds[i]))
+    before = arr[0]
     for i in range(1, len(arr)):
-        if arr[i] == arr[i-1]:
-            num += 1
+        if arr[i] < before:
+            arr[i] = before
         else:
-            answer.append(num)
-            num = 1
-            
-    answer.append(num)
-            
+            before = arr[i]
+    counter = Counter(arr)
+    answer = list(counter.values())
     return answer
