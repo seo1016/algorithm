bb0c303ec3993545372ad914cb8d65e2cb487b3d
서승훈 <seoseunghun3@gmail.com>
2025-07-03 15:07:38 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,12 +2,13 @@ def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
         merged = arr1[i] | arr2[i]
-        bin_str = bin(merged)[2:].zfill(n)
+        str_bin = bin(merged)[2:].zfill(n)
+        
         line = ''
-        for i in bin_str:
-            if i=='1':
-                line+='#'
+        for j in str_bin:
+            if j == '1':
+                line += '#'
             else:
-                line+=' '
+                line += ' '
         answer.append(line)
     return answer
